13e240dc0ba25c11fcb6072d57def1d4b805e231
Noël Schenkel <noggi10@hotmail.com>
2019-03-03 18:35:51 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Noël Schenkel on 03.03.19.
+//  Copyright © 2019 Noël Schenkel. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
